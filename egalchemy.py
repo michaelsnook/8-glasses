93eaf4b713e89da0378f696458b747a96bbea53b
michaelsnook <michael.snook@gmail.com>
2014-02-24 03:57:41 -0500

    Still working on SQLAlchemy...
    I think I’m now doing the add-one and delete-one views correctly, but I
    still can’t tell really. I’ll have to work on it later.

--- egalchemy.py
+++ egalchemy.py
@@ -1,6 +1,9 @@
 from datetime import datetime 
 import os, string
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import scoped_session, sessionmaker
+#from sqlalchemy.ext.declarative import declarative_base
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
@@ -19,6 +22,14 @@ app.config.update(dict(
 app.config.from_envvar('EIGHTGLASSES_SETTINGS', silent=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///egalchemy.db'
 db = SQLAlchemy(app)
+
+#engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+#db_session = scoped_session(sessionmaker(autocommit=False,
+#                                         autoflush=False,
+#                                         bind=engine))
+#Base = declarative_base()
+#Base.query = db_session.query_property()
+
 
 class User(db.Model):
   __tablename__ = 'users'
@@ -52,7 +63,7 @@ class Goal(db.Model):
     self.type = type
     self.direction = direction
     self.period = period
-    self.very = very
+    self.verb = verb
     self.subtitle = subtitle
   def __repr__(self):
     return '<Goal %r>' % self.name
@@ -82,8 +93,7 @@ class Entry(db.Model):
 def home():
   if not session.get('logged_in'):
     return render_template('index.html')
-  cur = db.execute('select id, name, total, type, datetime(created_at, "localtime") `created_at`, notes from entries order by id desc')
-  entries = cur.fetchall()
+  entries = Entry.query.filter().all()
   
   """ I'm structuring these queries to share the same "start" and "end" strings, so that it's 
       extra super clear what exactly is varying from one to the next.
@@ -98,13 +108,13 @@ def home():
   querystart = 'select max(entries.id) `max_entry_id`, datetime(max(entries.created_at), "localtime") `most_recent`, goals.id `goal_id`, goals.name, goals.type, goals.direction, goals.period, goals.verb, goals.subtitle, sum(coalesce(entries.total, 0)) `sum`, count(distinct entries.id) `count`, goals.goal,  datetime(goals.created_at, "localtime") `created_at` from goals left join entries on '
   queryend = 'group by goals.name'
   
-  cur_day = db.execute(querystart + '(entries.name=goals.name and date(entries.created_at, "localtime") = date("now", "localtime")) where goals.period="daily" ' + queryend)
+  cur_day = db.from_statement(querystart + '(entries.name=goals.name and date(entries.created_at, "localtime") = date("now", "localtime")) where goals.period="daily" ' + queryend)
   dailytotals = cur_day.fetchall()
 
-  cur_week = db.execute(querystart + '(entries.name=goals.name and date(entries.created_at, "localtime", "weekday 1", "-7 days") = date("now", "localtime", "weekday 1", "-7 days")) where goals.period="weekly" ' + queryend)
+  cur_week = db.from_statement(querystart + '(entries.name=goals.name and date(entries.created_at, "localtime", "weekday 1", "-7 days") = date("now", "localtime", "weekday 1", "-7 days")) where goals.period="weekly" ' + queryend)
   weeklytotals = cur_week.fetchall()
 
-  cur = db.execute(querystart + '(entries.name=goals.name) ' + queryend + ', goals.type order by goals.created_at asc')
+  cur = db.from_statement(querystart + '(entries.name=goals.name) ' + queryend + ', goals.type order by goals.created_at asc')
   totals = cur.fetchall()
   
   goaltotals = dailytotals + weeklytotals
@@ -120,26 +130,27 @@ def admin():
   "  kind of rather go through the work of building it myself to know that I'm doing 
   "  things in a way that will still feel good on a phone.
   """
-  
+
   if not session.get('logged_in'):
       abort(401)
- 
-  """ This one just returns every entry. """
-  cur = db.execute('select id, name, total, type, created_at, notes from entries order by id desc')
-  entries = cur.fetchall()
+
+  entries = Entry.query.filter().all()
 
   """ This one returns all-time totals for every goal. """
-  cur = db.execute('select goals.id `goal_id`, goals.name, goals.type, goals.direction, goals.period, goals.verb, goals.subtitle, sum(coalesce(entries.total, 0)) `sum`, count(distinct entries.id) `count`, goals.goal, max(entries.created_at) `most_recent`, goals.created_at from goals left join entries on (entries.name=goals.name) group by goals.name, goals.type order by goals.created_at asc')
-  totals = cur.fetchall()
+#  cur = db.session.from_statement('select goals.id `goal_id`, goals.name, goals.type, goals.direction, goals.period, goals.verb, goals.subtitle, sum(coalesce(entries.total, 0)) `sum`, count(distinct entries.id) `count`, goals.goal, max(entries.created_at) `most_recent`, goals.created_at from goals left join entries on (entries.name=goals.name) group by goals.name, goals.type order by goals.created_at asc')
+#  totals = cur.fetchall()
 
+# this is really not supposed to be happening #
+# it's just to avoid an error before rendering the template #
+  totals = entries 
+ 
   return render_template('admin.html', entries=entries, totals=totals, )
 
 @app.route('/numbers')
 def numbers():
   if not session.get('logged_in'):
     abort(401)
-  cur = db.execute('select id, name, total, type, created_at, notes from entries order by id desc')
-  entries = cur.fetchall()
+  entries = Entry.query.filter().all()
   return render_template('numbers.html', entries=entries, )
 
 
@@ -150,10 +161,10 @@ def add_entry():
   form = request.form
   
   entry = Entry(form['name'], form['goal_id'], form['total'], form['notes'])
-  db.add(entry)
+  db.session.add(entry)
 #  db.execute('insert into entries (name, total, notes) values (?, ?, ?)',
 #               [form['name'], form['total'], form['notes']])
-  db.commit()
+  db.session.commit()
   app.logger.debug(db.total_changes)
   if db.total_changes:
     flash('New entry was successfully posted')
@@ -173,9 +184,10 @@ def remove_entry():
         s = s + f[3:] + ','
     s = s[:-1]
     # i don't know if this will work
-    db.query(Entry).filter(Entry.id.in_(s.split(',')))
+    target = Entry.query.filter(Entry.id.in_(s.split(',')))
+    db.session.delete(target)
     # db.execute('DELETE FROM entries WHERE id in(' + s + ')')
-    db.commit()
+    db.session.commit()
     flash('Successfully deleted those pesky entries')
     return redirect(url_for('home'))
   else:
@@ -193,10 +205,10 @@ def add_goal():
 #  db.execute('insert into goals (name, goal, type, direction, period, verb, subtitle) values (?, ?, ?, ?, ?, ?, ?)',
 #               [form['name'], form['goal'], form['type'], form['direction'], form['period'], form['verb'], request.form['subtitle']])
 
-  db.add(goal)
-  db.commit()
+  db.session.add(goal)
+  db.session.commit()
   flash('Your new goal was successfully added!')
-  return redirect(url_for('home'))   
+  return redirect(url_for('admin'))   
     
 @app.route('/removegoal', methods=['POST'])
 def remove_goal():
@@ -206,10 +218,10 @@ def remove_goal():
   if form['delete'] == 'delete' and form['areyousure']:
     s = form['id_name'].split(',')
     
-    target = db.query(Goal).filter( id == s[0] , name == s[1] ).first()
-    db.delete(target)
+    target = Goal.query.filter( id == s[0] , name == s[1] ).first()
+    db.session.delete(target)
     #db.execute('DELETE FROM entries where name = ?', [s[1]])
-    db.commit()
+    db.session.commit()
     flash('Your goal was successfully deleted :(')
     return redirect(url_for('home'))
   else:
