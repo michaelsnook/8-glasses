50cb443263dc247a9f5ad1bb6ce240a91136d466
michaelsnook <michael.snook@gmail.com>
2014-03-07 00:19:40 -0500

    fixing settings for the public repo

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import os, string
 
-DB_URI = os.environ['DATABASE_URL']
-# DB_URI = 'postgresql://localhost/eightglasses'
+# DB_URI = os.environ['DATABASE_URL']
+DB_URI = 'postgresql://localhost/eightglasses'
