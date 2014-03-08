153be5a586e6fcce979c90ad6490f8223b29fe3c
michaelsnook <michael.snook@gmail.com>
2014-03-08 15:44:40 -0500

    trying again...

--- eightglasses.py
+++ eightglasses.py
@@ -16,14 +16,12 @@ app.config.update(dict(
 ))
 app.config.from_envvar('EIGHTGLASSES_SETTINGS', silent=True)
 
-# for local database. change this in production
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/eightglasses'
-
 # on heroku you have a database_url on os.environ
 if hasattr(os.environ, 'DATABASE_URL'):
-  app.config.update(dict(
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'], 
-  ))
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# for local database. change this in production
+else:
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/eightglasses'
 
 # now that we've config'd our app... get the db object
 db = SQLAlchemy(app)
